2dd48ea011cf3ee9faff6c055aac34ac1c309b49
TerrenceMM2 <terrencemm2@gmail.com>
2019-05-19 18:08:28 -0500

    adds logic to determine correct or incorrect answer based on store data-values

--- README.md
+++ README.md
@@ -3,8 +3,10 @@ Vanderbilt Coding Boot Camp - Coursework 5 - Trivia Game
 Live Link: Coming Soon ...  
   
 ### Pseudocode  
-1. User clicks "start" to begin  
+1. User clicks "start" to begin `startGame()`   
 2. First question is displays  
+    * Randomize question  
+    * Randomize answer placement  
     * 30 second timer starts  
     `setInterval()` 
 3. User selects an answer  
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,116 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-var questions = {
-    1 : {
-        question: "What is the name of the coffee shop the friends hang out at?",
-        1: "Central Perk",
-        2: "Starbucks",
-        3: "Central Joe's",
-        4: "Coffee Corner"
-    },
-    2 : {
-        question: "Who was Ross' first wife?",
-        1: "Emily",
-        2: "Rachel",
-        3: "Susan",
-        4: "Carol"
-    } 
-}
+    var questionArray = [{
+            question: "What is the name of the coffee shop the friends hang out at?",
+            1: ["Central Perk", true],
+            2: ["Starbucks", false],
+            3: ["Central Joe's", false],
+            4: ["Coffee Corner", false]
+        },
+        {
+            question: "Who was Ross' first wife?",
+            1: ["Emily", false],
+            2: ["Rachel", false],
+            3: ["Susan", false],
+            4: ["Carol", true],
+        },
+        {
+            question: "What was the name of Pheobe's hit single?",
+            1: ["Pervert Parade", false],
+            2: ["Smelly Cat", true],
+            3: ["Sticky Shoes", false],
+            4: ["Lather, Rinse, Repeat", false]
+        }
+    ];
 
-var correctGif = ["correct1.gif", "correct2.gif", "correct3.gif", "correct4.gif", "correct5.gif", "correct6.gif", "correct7.gif", "correct8.gif", "correct9.gif", "correct10.gif", "correct11.gif", "correct12.gif", "correct13.gif", "correct14.gif", "correct15.gif"];
-var incorrectGif = ["incorrect1.gif", "incorrect2.gif", "incorrect3.gif", "incorrect4.gif", "incorrect5.gif", "incorrect6.gif", "incorrect7.gif", "incorrect8.gif", "incorrect9.gif", "incorrect10.gif", "incorrect11.gif", "incorrect12.gif", "incorrect13.gif", "incorrect15.gif", "incorrect15.gif"];
-var questionsAsked = 0;
+    var correctGif = ["correct1.gif", "correct2.gif", "correct3.gif", "correct4.gif", "correct5.gif", "correct6.gif", "correct7.gif", "correct8.gif", "correct9.gif", "correct10.gif", "correct11.gif", "correct12.gif", "correct13.gif", "correct14.gif", "correct15.gif"];
+    var incorrectGif = ["incorrect1.gif", "incorrect2.gif", "incorrect3.gif", "incorrect4.gif", "incorrect5.gif", "incorrect6.gif", "incorrect7.gif", "incorrect8.gif", "incorrect9.gif", "incorrect10.gif", "incorrect11.gif", "incorrect12.gif", "incorrect13.gif", "incorrect15.gif", "incorrect15.gif"];
+    var questionsAsked = 0;
+    var correctAnswers = 0;
+    var incorrectAnswers = 0;
+    var unanswered = 0;
+    var timer = 30;
 
-$(".start").on("click", function() {
-    startGame();
-});
+    $(".start").on("click", function () {
+        startGame();
+        randomQuestion();
+        setInterval(countDown, 1000, 31);
+    });
 
-function startGame() {
-    $("#start").fadeOut().hide();
-    $("#questions").fadeIn().show();
-}
+    $(".answer").on("click", function () {
+        var answerBoolean = $(this).data("value");
+        if (answerBoolean) {
+            var randomGif = Math.floor(Math.random() * correctGif.length);
+            var gifUrl = "assets/images/" + correctGif[randomGif];
+            $("#result-text").text("Correct!");
+            $("#result-text").attr("id", "correct");
+            $("#gif").attr("src", gifUrl);
+        } else {
+            var randomGif = Math.floor(Math.random() * incorrectGif.length);
+            var gifUrl = "assets/images/" + incorrectGif[randomGif];
+            $("#result-text").text("Wrong!");
+            $("#result-text").attr("id", "incorrect");
+            $("#gif").attr("src", gifUrl);
+        }
+        $("#question-container").fadeOut().hide();
+        $("#result-container").fadeIn().show();
+    });
+
+    function startGame() {
+        $("#start").fadeOut().hide();
+        $("#question-container").fadeIn().show();
+    };
+
+    function randomQuestion() {
+        var randomQuestionIndex = Math.floor(Math.random() * questionArray.length); // 1
+        // var nextIndex = randomQuestionIndex + 1;
+        var randomQuestion = questionArray.splice(randomQuestionIndex, 1);
+        $("#question").text(randomQuestion[0].question);
+        $("#answer1").text(randomQuestion[0][1][0]);
+        $("#answer1").attr("data-value", randomQuestion[0][1][1]);
+        $("#answer2").text(randomQuestion[0][2][0]);
+        $("#answer2").attr("data-value", randomQuestion[0][2][1]);
+        $("#answer3").text(randomQuestion[0][3][0]);
+        $("#answer3").attr("data-value", randomQuestion[0][3][1]);
+        $("#answer4").text(randomQuestion[0][4][0]);
+        $("#answer4").attr("data-value", randomQuestion[0][4][1]);
+    };
+
+    function nextQuestion() {
+        $("#result-container").fadeOut().hide();
+        $("#question-container").fadeIn().show();
+    }
+
+    // setInterval to fire "x" number of times.
+    // Source: https://stackoverflow.com/questions/2956966/javascript-telling-setinterval-to-only-fire-x-amount-of-times
+    function setInterval(callback, delay, repetitions) {
+        var x = 0;
+        var intervalID = window.setInterval(function () {
+            callback();
+            if (++x === repetitions) {
+                window.clearInterval(intervalID);
+            }
+        }, delay);
+    };
+
+    // Decrements timer variable. Adds styling if less than 10 and less than 5 secions.
+    // If the time reaches 0, unanswered value increases by 1.
+    function countDown() {
+        $("#timer").text(timer);
+        if (timer === 0) {
+            clearInterval(timer);
+            unanswered++;
+            console.log(unanswered);
+        } else if (timer < 11 && timer > 6) {
+            $("#timer").css("color", "#fd7e14")
+            timer--;
+        } else if (timer < 6) {
+            $("#timer").css("color", "#dc3545");
+            timer--;
+        } else {
+            timer--;
+        };
+        return timer;
+    };
 
 });
